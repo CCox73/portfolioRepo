7305521bc780ea672b982ee398c5c3c470a0d71c
Connor Cox <connor.cox1@csuglobal.edu>
2024-03-07 18:15:15 -0700

    Added exception handling

--- VehicleInventory/src/automobile.java
+++ VehicleInventory/src/automobile.java
@@ -10,83 +10,126 @@ public class automobile {
 	
 	//Default constructor
 	public automobile() {
-		make = "DefaultMake";
-		model = "DefaultModel";
-		color = "DefaultColor";
-		year = 0;
-		mileage = 0;
-		System.out.println("Default vehicle created");
+		try {
+			make = "DefaultMake";
+			model = "DefaultModel";
+			color = "DefaultColor";
+			year = 9999;
+			mileage = 0;
+			System.out.println("Default vehicle created");
+		}
+		catch (Exception excpt) {
+			System.out.println(excpt.getMessage());
+			System.out.println("Default construction failed");
+		}
 	}
 	
 	//Parameterized constructor
 	public automobile(String carMake, String carModel, String carColor, int carYear, int carMileage) {
-		make = carMake;
-		model = carModel;
-		color = carColor;
-		year = carYear;
-		mileage = carMileage;
-		System.out.println("Parameterized vehicle created");
+		try {
+			make = carMake;
+			model = carModel;
+			color = carColor;
+			year = carYear;
+			mileage = carMileage;
+			System.out.println("Parameterized vehicle created");
+		}
+		catch (Exception excpt) {
+			System.out.println(excpt.getMessage());
+			System.out.println("Parameterized construction failed");
+		}
 	}
 	
 	//List Vehicle Info
 	public String[] getInfo() {
-		String[] list = {make, model, color, year + "", mileage + ""};
-		System.out.println("Variable retrieved");
-		return list;
+		try {
+			String[] list = {make, model, color, year + "", mileage + ""};
+			System.out.println("Variable retrieved");
+			return list;
+		}
+		catch (Exception excpt) {
+			System.out.println(excpt.getMessage());
+			System.out.println("Vehicle info retrieval failed");
+			return null;
+		}
 	}
 	
 	//Remove vehicle (Objects are deleted automatically through garbage collection,
 	//this just sets all of its values to 0 or null)
 	public void removeVehicle() {
-		make = null;
-		model = null;
-		color = null;
-		year = 0;
-		mileage = 0;
-		System.out.println("Vehicle removed");
+		try {
+			make = null;
+			model = null;
+			color = null;
+			year = 0;
+			mileage = 0;
+			System.out.println("Vehicle removed");
+		}
+		catch (Exception excpt) {
+			System.out.println(excpt.getMessage());
+			System.out.println("Vehicle removal failed");
+		}
 	}
 	
 	//Update vehicle attributes (all at once)
 	public void updateInfo(String newMake, String newModel, String newColor, int newYear, int newMileage) {
-		make = newMake;
-		model = newModel;
-		color = newColor;
-		year = newYear;
-		mileage = newMileage;
-		System.out.println("All vehicle info updated");
+		try {
+			make = newMake;
+			model = newModel;
+			color = newColor;
+			year = newYear;
+			mileage = newMileage;
+			System.out.println("All vehicle info updated");
+		}
+		catch (Exception excpt) {
+			System.out.println(excpt.getMessage());
+			System.out.println("Vehicle info update failed");
+		}
 	}
 	//Update vehicle attributes (one at a time, for String info)
 	public void updateInfo(int choice, String newInfo) {
-		switch (choice) {
-		case 0:
-			make = newInfo;
-			System.out.println("Vehicle make updated");
-			break;
-		case 1:
-			model = newInfo;
-			System.out.println("Vehicle model updated");
-			break;
-		case 2:
-			color = newInfo;
-			System.out.println("Vehicle color updated");
-			break;
-		default:
-			System.out.println("Error: 0-2 require String input, 3-4 require int input");
+		try {
+			switch (choice) {
+			case 0:
+				make = newInfo;
+				System.out.println("Vehicle make updated");
+				break;
+			case 1:
+				model = newInfo;
+				System.out.println("Vehicle model updated");
+				break;
+			case 2:
+				color = newInfo;
+				System.out.println("Vehicle color updated");
+				break;
+			default:
+				System.out.println("Error: 0-2 require String input, 3-4 require int input");
+			}
+		}
+		catch (Exception excpt) {
+			System.out.println(excpt.getMessage());
+			System.out.println("Vehicle info update failed");
 		}
 	}
 	//Update vehicle attributes (one at a time, for int info)
 	public void updateInfo(int choice, int newInfo) {
-		switch (choice) {
-		case 3:
-			year = newInfo;
-			System.out.println("Vehicle year updated");
-			break;
-		case 4:
-			mileage = newInfo;
-			System.out.println("Vehicle mileage updated");
-			break;
-		default:
-			System.out.println("Error: 0-2 require String input, 3-4 require int input");
+		try {
+			switch (choice) {
+			case 3:
+				year = newInfo;
+				System.out.println("Vehicle year updated");
+				break;
+			case 4:
+				mileage = newInfo;
+				System.out.println("Vehicle mileage updated");
+				break;
+			default:
+				System.out.println("Error: 0-2 require String input, 3-4 require int input");
+			}
+		}
+		catch (Exception excpt) {
+			System.out.println(excpt.getMessage());
+			System.out.println("Vehicle info update failed");
 		}
 	}
 }
--- VehicleInventory/src/vehicleInventory.java
+++ VehicleInventory/src/vehicleInventory.java
@@ -1,17 +1,18 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 public class vehicleInventory {
 
 	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
 		
 		//"Call automobile class with parameterized constructor (e.g., "make, model, color, year, mileage")."
 		automobile car1 = new automobile("Nissan", "Leaf", "White", 2012, 15000);
 		
 		//"Then call the method to list the values. Loop through the array and print to the screen."
 		for (int i = 0; i < 5; i++) {
-			System.out.println(">"+car1.getInfo()[i]);
+			System.out.println(">" + car1.getInfo()[i]);
 		}
-		
-		System.out.println(">"+printInfo(car1));
+		//Demonstration compared to output with loop^
+		printInfo(car1);
 		
 		//"Call the remove vehicle method to clear the variables"
 		car1.removeVehicle();
@@ -20,19 +21,40 @@ public class vehicleInventory {
 		//This should be using an 'add new vehicle' method. How is that different from a constructor?
 		//Should the method be in automobile or vehicleInventory? Should I be putting these in an array?
 		automobile car2 = new automobile();
-		System.out.println(">"+printInfo(car2));
+		printInfo(car2);
 		
-		//"Update the vehicle."
+		//"Update the vehicle... Call the listing method and print the information to the screen."
 		car2.updateInfo("Toyota", "Corolla", "Silver", 2007, 23000);
-		System.out.println(">"+printInfo(car2));
+		printInfo(car2);
 		car2.updateInfo(2, "Grey");
-		System.out.println(">"+printInfo(car2));
+		printInfo(car2);
+		
+		//"Display a message asking if the user wants to print the information to a file (Y or N)."
+		System.out.println("Would you like to print the information to a file? (Y/N):");
+		String userInput = scnr.next();
+		switch (userInput) {
+		case "Y":
+			System.out.println("Y: Printing information to [placeholder].txt...");
+			break;
+		case "N":
+			System.out.println("N: Information will not be printed to file.");
+			break;
+		default:
+			System.out.println("Invalid selection: Information will not be printed to file.");
+		}
+		
+		scnr.close();
 	}
 	
-	//Takes automobile input, gives String output of preformatted info using list method
-	public static String printInfo(automobile inputCar) {
-		String output = inputCar.getInfo()[3] + " " + inputCar.getInfo()[0] + " " + inputCar.getInfo()[1] + ", " + inputCar.getInfo()[2] + ", " + inputCar.getInfo()[4] + " miles";
-		System.out.println("Vehicle info formatted");
-		return output;
+	//Takes automobile input, prints String output of organized info using list method
+	public static void printInfo(automobile inputCar) {
+		try {
+			String output = ">" + inputCar.getInfo()[3] + " " + inputCar.getInfo()[0] + " " + inputCar.getInfo()[1] + ", " + inputCar.getInfo()[2] + ", " + inputCar.getInfo()[4] + " miles";
+			System.out.println(output);
+		}
+		catch (Exception excpt) {
+			System.out.println(excpt.getMessage());
+			System.out.println("Info could not be printed");
+		}
 	}
 }
